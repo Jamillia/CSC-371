bf9f3e0ab6e74aa69529cb14c20db3abba90c49f
Jamillia <dbass999@gmail.com>
2020-01-21 18:41:34 -0800

    1/21c

---
+++ Test1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float groundSpeed = 10f;
+    public float airSpeed = 1f;
+    private float airSpeedMultiplier = 1f;
+    public float jumpHeight = 5f;
+    public float maxVelocity = 10f;
+    Rigidbody rb;
+    bool isgrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = x * transform.right + z * transform.forward;
+        bool jump = Input.GetButtonDown("Jump");
+        if (isgrounded)
+        {
+            rb.velocity = move * groundSpeed + rb.velocity.y * transform.up;
+            if (jump)
+            {
+                rb.velocity += transform.up * jumpHeight;
+            }
+        }
+        else
+        {
+            if (Mathf.Abs((move * airSpeed * airSpeedMultiplier +
+                transform.right * rb.velocity.x + transform.forward * rb.velocity.z).magnitude)
+                <= maxVelocity)
+            {
+                rb.velocity += move * airSpeed * airSpeedMultiplier;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isgrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isgrounded = false;
+        }
+    }
+}
---
+++ Test1/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    public float rocketLifeTime = 10f;
+    public float rocketSpeed = 35f;
+    public float explosionRadius = 2.7f;
+    public float explosionPower = 10f;
+    GameObject moveTo;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        moveTo = GameObject.FindWithTag("GoTo");
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, rocketLifeTime);
+        rb.velocity = rocketSpeed * (moveTo.transform.position - transform.position).normalized;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+            foreach (Collider hit in Physics.OverlapSphere(transform.position, explosionRadius))
+            {
+                if (hit.CompareTag("Player"))
+                {
+                    Rigidbody hitrb = hit.GetComponentInParent<Rigidbody>();
+                    hitrb.AddExplosionForce(explosionPower, transform.position, explosionRadius,
+                        1f, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
