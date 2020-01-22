4497b7fa5c8bdf0d658fd2ef9df21ea43ebb155e
Jamillia <dbass999@gmail.com>
2020-01-21 18:42:08 -0800

    1/21d

---
+++ Create With Code Tutorial/New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float horizInput;
+    public float speed = 10f;
+    public float xRange = 20f;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < -xRange || transform.position.x > xRange)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xRange, xRange),
+                transform.position.y, transform.position.z);
+        }
+        transform.Translate(Vector3.right * horizInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab,
+                transform.position,
+                projectilePrefab.transform.rotation);
+        }
+    }
+}
